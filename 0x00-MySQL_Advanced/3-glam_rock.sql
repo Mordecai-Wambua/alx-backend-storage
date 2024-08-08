2477f20fb8529870d3f1f463f4fa5e3751792c98
Mordecai-Wambua <mordecaingumbau@gmail.com>
2024-08-08 18:14:07 +0300

    Task 03

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,7 @@
+-- Lists all bands with Glam rock as their main style, ranked by their longevity
+SELECT
+	band_name,
+	COALESCE(split, 2020) - formed AS lifespan
+FROM metal_bands
+WHERE FIND_IN_SET('Glam rock', style) > 0
+ORDER BY lifespan DESC;
