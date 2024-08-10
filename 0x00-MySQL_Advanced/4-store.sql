db5268cc91c260ef7d364b8f4f898896cdb01927
Mordecai-Wambua <mordecaingumbau@gmail.com>
2024-08-10 16:26:42 +0300

    Task 04: Triggers

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,13 @@
+-- A trigger to descrease the quantity of an item after adding a new order
+DELIMITER $$
+
+CREATE TRIGGER decrease
+AFTER INSERT ON orders
+FOR EACH ROW
+	BEGIN
+		UPDATE items
+		SET quantity = quantity - NEW.number
+		WHERE name = NEW.item_name;
+	END $$
+
+DELIMITER ;
