96acbd23e12b8f4e60076f817721d919a5194f80
Mordecai-Wambua <mordecaingumbau@gmail.com>
2024-08-13 18:33:18 +0300

    Task 14

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -4,13 +4,25 @@
 
 def top_students(mongo_collection):
     """Return sorted student list."""
-    for doc in mongo_collection.find():
-        average = 0.0
-        x = 0.0
-        for scores in doc.get('topics'):
-            x += scores['score']
-        average = x / 3
-        update = {'$set': {'averageScore': average}}
-        mongo_collection.update_one({'_id': doc['_id']}, update)
+    pipeline = [
+        {
+            '$addFields': {
+                'averageScore': {'$avg': '$topics.score'}
+            }
+        },
+        {
+            '$sort': {'averageScore': -1}
+        }
+    ]
 
-    return mongo_collection.find().sort('averageScore', -1)
+    return list(mongo_collection.aggregate(pipeline))
+    # for doc in mongo_collection.find():
+    #     average = 0.0
+    #     x = 0.0
+    #     for scores in doc.get('topics'):
+    #         x += scores['score']
+    #     average = x / 3
+    #     update = {'$set': {'averageScore': average}}
+    #     mongo_collection.update_one({'_id': doc['_id']}, update)
+
+    # return mongo_collection.find().sort('averageScore', -1)
