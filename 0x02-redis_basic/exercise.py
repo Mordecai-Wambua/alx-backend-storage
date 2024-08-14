d16047ac8d0640fd0c905b6de85c31fd1f736c02
Mordecai-Wambua <mordecaingumbau@gmail.com>
2024-08-14 17:59:34 +0300

    Task 00

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -9,11 +9,11 @@ class Cache:
 
     def __init__(self) -> None:
         """Instantiate the redis client."""
-        self.__redis = redis.Redis()
-        self.__redis.flushdb()
+        self._redis = redis.Redis()
+        self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Input data into redis using the generated a random key."""
         key: str = str(uuid.uuid4())
-        self.__redis.set(key, data)
+        self._redis.set(key, data)
         return key
